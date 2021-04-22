31b7ad697487e7558c6a2f2ff7412257e1b2ffd2
ross <ross.russell@protonmail.com>
2021-04-21 18:16:32 -0700

    express

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// express after build.
+
 app.use(express.static('build'));
 app.listen(port, (err, result)=>{
     console.log('listening on 3000');
